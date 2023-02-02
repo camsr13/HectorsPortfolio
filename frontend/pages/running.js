8eab0c6c5ba6ee82b03c0ac88f5cff2a91e0bb83
Fergus  Haywood <fergus.haywood5@gmail.com>
2023-02-03 08:55:33 +1300

    remove the strava api and move data fetching to running page

--- frontend/pages/running.js
+++ frontend/pages/running.js
@@ -1,4 +1,5 @@
 import styles from '../styles/Running.module.css'
+import { router } from 'next/router'
 
 export default function Running({ data }) {
   const runningData = data.all_run_totals
@@ -14,12 +15,46 @@ export default function Running({ data }) {
 }
 
 export async function getStaticProps() {
-  const data = await fetch('https://fergushaywood.com/api/strava').then((res) =>
-    res.json()
-  )
+  async function getAccessToken() {
+    const client_id = process.env.STRAVA_CLIENT_ID
+    const client_secret = process.env.STRAVA_CLIENT_SECRET
+    const refresh_token = process.env.STRAVA_REFRESH_TOKEN
+
+    const headers = {
+      Accept: 'application/json text/plain, */*"',
+      'Content-Type': 'application/json',
+    }
+
+    const body = JSON.stringify({
+      client_id,
+      client_secret,
+      refresh_token,
+      grant_type: 'refresh_token',
+    })
+
+    const authorize = await fetch('https://www.strava.com/oauth/token', {
+      headers,
+      method: 'POST',
+      body,
+    }).then((res) => res.json())
+
+    return authorize.access_token
+  }
+
+  const response = async () => {
+    const athleteId = 63544607
+    const access_token = await getAccessToken()
+
+    return await fetch(
+      `https://www.strava.com/api/v3/athletes/${athleteId}/stats?access_token=${access_token}`
+    ).then((res) => res.json())
+  }
+
+  const data = await response()
+
   return {
     props: {
-      data: data,
+      data,
     },
   }
 }
